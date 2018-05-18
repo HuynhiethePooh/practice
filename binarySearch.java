1489f6500d03e7f155a336e18d2d18b565a9befd
HuynhiethePooh <36939074+HuynhiethePooh@users.noreply.github.com>
2018-05-18 16:54:27 -0500

    Create binarySearch.java

---
+++ binarySearch.java
@@ -0,0 +1,21 @@
+   int binarySearch(int[] a, int x){
+         int low = 0;
+         int high = a.length - 1;
+         int mid;
+         
+         while (low <= high){
+             mid = (low + high) / 2;
+             
+             if (x < a[mid]){
+                 high = mid - 1;
+             }
+             else if ( x > a[mid]){
+                 low = mid + 1;
+             }
+             else { 
+                 return mid;
+            }
+         }
+         return -1; // couldn't find it
+     }
+         
